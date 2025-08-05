eb02dc5d1036d7b4a1a97a7c35b5cdb56e0361f7
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-08-05 13:50:47 +0800

    Implementasi ViewModel

--- app/src/main/java/com/example/todolist/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/todolist/ui/home/HomeScreen.kt
@@ -3,12 +3,14 @@ package com.example.todolist.ui.home
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -16,11 +18,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.todolist.ui.TodoAppViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen (
-
+    viewModel: TodoAppViewModel
 ) {
     Scaffold (
       topBar = {
@@ -41,15 +44,18 @@ fun HomeScreen (
     ) {
         innerPadding ->
             LazyColumn(modifier = Modifier.padding(innerPadding)) {
-                items(count = 1) {
+                items(viewModel.tasks) { task ->
                     Card(
                         modifier = Modifier
                                 .padding(8.dp)
                             .clickable {
-
+                                // TODO
                             }
                     ) {
-                        Text(text = "hello")
+                        Text(text = task.title,
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.padding(16.dp)
+                        )
                     }
                 }
             }
@@ -59,5 +65,5 @@ fun HomeScreen (
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    HomeScreen(viewModel = TodoAppViewModel())
 }
